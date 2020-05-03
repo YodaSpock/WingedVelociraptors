8e658592bcb078de568c0d985c028087f0995f5c
Isaac Spanier <ispanier@iastate.edu>
2020-05-03 04:16:33 -0500

    resolved Daniel's pull request comments

--- client/src/Pages/LoginForm.jsx
+++ client/src/Pages/LoginForm.jsx
@@ -5,16 +5,16 @@ import { Input, Button, Row, Col} from 'antd';
 //import { Link } from 'react-router-dom';
 
 const LoginForm = () => (
-        <Formik
+    <Formik
         initialValues={{
             name: ''
         }}
         validationSchema={Yup.object({
             name: Yup
-            .string()
-            .min(4, "Put your full name Catherine...")
-            .max(15, "Very funny, please put just your first name" )
-            .required("You forgot your name... you fool!")
+                .string()
+                .min(4, "Put your full name Catherine...")
+                .max(15, "Very funny, please put just your first name" )
+                .required("You forgot your name... you fool!")
         })}
         onSubmit={(values, {setSubmitting, resetForm}) => {
             console.log(values.name);
@@ -35,8 +35,7 @@ const LoginForm = () => (
                 /*Other Things*/
             })=>(
                 <form onSubmit = {handleSubmit}>
-                    <Row style = {{paddingTop: "3vh", paddingBottom: "3vh"}}>
-                    <Col xs = {6} md = {8}/>
+                    <Row style = {{paddingTop: "3vh", paddingBottom: "3vh", justifyContent: "center"}}>
                         <Col xs = {12} md = {8} style = {{textAlign: "center", fontFamily: "minecraft", fontSize: "100%"}}>
                             <Input
                             id = "name" 
@@ -47,28 +46,24 @@ const LoginForm = () => (
                             placeholder = "Player Name" 
                             style = {{textAlign: "center"}}
                             />
-                            {errors.name && touched.name && errors.name}
+                            {touched.name && errors.name}
                         </Col>
-                        <Col xs = {6} md = {8}/>
                     </Row>
                     
-                    <Row style = {{paddingTop: "3vh", paddingBottom: "3vh"}}>
-                        <Col xs = {0} md = {8}/>
-                        <Col xs = {24} md = {8} style = {{textAlign: "center", fontFamily: "minecraft", fontSize: "100%"}}>
+                    <Row style = {{paddingTop: "3vh", paddingBottom: "3vh", justifyContent: "center"}}>
+                        <Col xs = {24} md = {12} style = {{textAlign: "center", fontFamily: "minecraft", fontSize: "100%"}}>
                             {/* TODO -  ADD THE START GAME FUNCTION */}
                             <Button htmlType = "submit" style = {{borderRadius: "50%", width: "30vh"}}>
                                 {/* <Link to = "/player/1/waiting"> */}
-                                
                                     Login
                                 {/* </Link> */}
                             </Button>
                         </Col>
-                        <Col xs = {0} md = {8}/>
                     </Row>
                     
                 </form>
             )}
-        </Formik>
+    </Formik>
 );
 
 export default LoginForm;

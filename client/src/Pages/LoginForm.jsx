eab598cb849e68b57cdedca2dffee7e5119b3b98
Isaac Spanier <ispanier@iastate.edu>
2020-05-02 21:13:52 -0500

    added yup validation

--- client/src/Pages/LoginForm.jsx
+++ client/src/Pages/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Formik} from 'formik';
 import * as Yup from 'yup';
-import { Input, Button} from 'antd';
+import { Input, Button, Row, Col} from 'antd';
 //import { Link } from 'react-router-dom';
 
 const LoginForm = () => (
@@ -13,8 +13,8 @@ const LoginForm = () => (
             name: Yup
             .string()
             .min(4, "Put your full name Catherine...")
-            .max(15, "I didn't mean your entire full name" )
-            .required("You forgot your name, you fool!")
+            .max(15, "Very funny, please put just your first name" )
+            .required("You forgot your name... you fool!")
         })}
         onSubmit={(values, {setSubmitting, resetForm}) => {
             console.log(values.name);
@@ -22,20 +22,22 @@ const LoginForm = () => (
                 alert(JSON.stringify(values, null, 2));
                 resetForm();
                 setSubmitting(false);
-            }, 3000)
+            }, 1000)
         }}
         >
             {({
                 values,
+                errors,
+                touched,
                 handleChange,
                 handleBlur,
                 handleSubmit,
                 /*Other Things*/
             })=>(
                 <form onSubmit = {handleSubmit}>
-                    {/* <Row style = {{paddingTop: "3vh", paddingBottom: "3vh"}}>
+                    <Row style = {{paddingTop: "3vh", paddingBottom: "3vh"}}>
                     <Col xs = {6} md = {8}/>
-                        <Col xs = {12} md = {8} style = {{textAlign: "center", fontFamily: "minecraft", fontSize: "100%"}}> */}
+                        <Col xs = {12} md = {8} style = {{textAlign: "center", fontFamily: "minecraft", fontSize: "100%"}}>
                             <Input
                             id = "name" 
                             name = "name" 
@@ -45,22 +47,24 @@ const LoginForm = () => (
                             placeholder = "Player Name" 
                             style = {{textAlign: "center"}}
                             />
-                        {/* </Col>
+                            {errors.name && touched.name && errors.name}
+                        </Col>
                         <Col xs = {6} md = {8}/>
-                    </Row> */}
+                    </Row>
                     
-                    {/* <Row style = {{paddingTop: "3vh", paddingBottom: "3vh"}}>
+                    <Row style = {{paddingTop: "3vh", paddingBottom: "3vh"}}>
                         <Col xs = {0} md = {8}/>
-                        <Col xs = {24} md = {8} style = {{textAlign: "center", fontFamily: "minecraft", fontSize: "100%"}}> */}
+                        <Col xs = {24} md = {8} style = {{textAlign: "center", fontFamily: "minecraft", fontSize: "100%"}}>
                             {/* TODO -  ADD THE START GAME FUNCTION */}
-                            <Button type = "submit" style = {{borderRadius: "50%", width: "30vh"}}>
+                            <Button htmlType = "submit" style = {{borderRadius: "50%", width: "30vh"}}>
                                 {/* <Link to = "/player/1/waiting"> */}
+                                
                                     Login
                                 {/* </Link> */}
                             </Button>
-                        {/* </Col>
+                        </Col>
                         <Col xs = {0} md = {8}/>
-                    </Row> */}
+                    </Row>
                     
                 </form>
             )}

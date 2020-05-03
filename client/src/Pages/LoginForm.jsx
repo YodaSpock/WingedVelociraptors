67921b5d0e5d59e0388b8626986fffd576501efb
Isaac Spanier <ispanier@iastate.edu>
2020-05-02 20:21:06 -0500

    tyring to get formik to cooperate with antd

--- client/src/Pages/LoginForm.jsx
+++ client/src/Pages/LoginForm.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Formik} from 'formik';
 import * as Yup from 'yup';
-import { Input, Button } from 'antd';
+import { Input, Button} from 'antd';
+//import { Link } from 'react-router-dom';
 
 const LoginForm = () => (
-        <Formik
+        <FormikForm
         initialValues={{
             name: ''
         }}
@@ -16,6 +17,7 @@ const LoginForm = () => (
             .required("You forgot your name, you fool!")
         })}
         onSubmit={(values, {setSubmitting, resetForm}) => {
+            console.log(values.name);
             setTimeout(()=>{
                 alert(JSON.stringify(values, null, 2));
                 resetForm();
@@ -25,30 +27,44 @@ const LoginForm = () => (
         >
             {({
                 values,
-                errors,
-                touched,
                 handleChange,
                 handleBlur,
                 handleSubmit,
-                isSubmitting,
                 /*Other Things*/
             })=>(
                 <form onSubmit = {handleSubmit}>
-                    <Input
-                    id = "name" 
-                    name = "name" 
-                    onChange = {handleChange}
-                    onBlur = {handleBlur}
-                    value = {values.name}
-                    placeholder = "Player Name" 
-                    style = {{textAlign: "center"}}
-                    />
-                    <Button style = {{borderRadius: "50%", width: "30vh"}}>
-                        Login
-                    </Button>
+                    {/* <Row style = {{paddingTop: "3vh", paddingBottom: "3vh"}}>
+                    <Col xs = {6} md = {8}/>
+                        <Col xs = {12} md = {8} style = {{textAlign: "center", fontFamily: "minecraft", fontSize: "100%"}}> */}
+                            <Input
+                            id = "name" 
+                            name = "name" 
+                            onChange = {handleChange}
+                            onBlur = {handleBlur}
+                            value = {values.name}
+                            placeholder = "Player Name" 
+                            style = {{textAlign: "center"}}
+                            />
+                        {/* </Col>
+                        <Col xs = {6} md = {8}/>
+                    </Row> */}
+                    
+                    {/* <Row style = {{paddingTop: "3vh", paddingBottom: "3vh"}}>
+                        <Col xs = {0} md = {8}/>
+                        <Col xs = {24} md = {8} style = {{textAlign: "center", fontFamily: "minecraft", fontSize: "100%"}}> */}
+                            {/* TODO -  ADD THE START GAME FUNCTION */}
+                            <Button type = "submit" style = {{borderRadius: "50%", width: "30vh"}}>
+                                {/* <Link to = "/player/1/waiting"> */}
+                                    Login
+                                {/* </Link> */}
+                            </Button>
+                        {/* </Col>
+                        <Col xs = {0} md = {8}/>
+                    </Row> */}
+                    
                 </form>
             )}
-        </Formik>
+        </FormikForm>
 );
 
 export default LoginForm;

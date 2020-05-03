b206d37d72a1386136676e7c3ef9cff8478d78cf
Isaac Spanier <ispanier@iastate.edu>
2020-05-03 15:35:04 -0500

    setup modal typography struture

--- client/src/Pages/RulesPage.jsx
+++ client/src/Pages/RulesPage.jsx
@@ -1,11 +1,19 @@
-import React from 'react' 
+import React from 'react'; 
+import {Typography} from 'antd'
+const {Title} = Typography; 
 
 export default class RulesPage extends React.Component{
 
     render(){
         return(
             <>
-            HELLO THERE
+            <Title level = {4}>
+                Gameplay
+            </Title>
+            <p>
+                This game is very similar to One Night Ultimate Werewolf
+            </p>
+            
             </>
 
         )

00c7fd7041dc7e3738f0fcebf822e8e164eef023
danielisgr8 <schubertmdaniel@gmail.com>
2020-05-07 14:58:15 -0500

    `GameApp.cleanUp` implementation.

--- server/src/app/game.js
+++ server/src/app/game.js
@@ -12,48 +12,21 @@ class GameApp {
     this.gameModule = gameModule;
 
     this.actTimer = null;
+
+    this.actHandler = this.actHandler.bind(this);
+    this.narrAckHandler = this.narrAckHandler.bind(this);
   }
 
   run() {
     this.nextAct();
 
-    this.wsem.addEventHandler(events.c_act, (id, data) => {
-      let responseData;
-      try {
-        responseData = this.gameModule.idAct(id, data.data);
-      } catch(error) {
-        this.wsem.sendMessage(id, events.s_error, { message: error.message });
-      }
-      if(responseData) this.wsem.sendMessage(id, events.s_act, { data: responseData });
-    });
-  
-    this.wsem.addEventHandler(events.c_narr_ack, () => {
-      if(this.actTimer) return;
+    this.wsem.addEventHandler(events.c_act, this.actHandler);
+    this.wsem.addEventHandler(events.c_narr_ack, this.narrAckHandler);
+  }
 
-      this.actTimer = setTimeout(() => {
-        this.actTimer = null;
-
-        if(this.gameModule.currentRole === roles.rachel) {
-          this.gameModule.players.filter((player) => player.originalRole === roles.rachel)
-            .forEach((player) => {
-              if(!player.asleep) this.wsem.sendMessage(player.id, events.s_act, { data: { noise: true } })
-            });
-        }
-
-        // TODO: add `state` prop to `s_act` that is either `start`, `end`, or `mid`
-        // TODO: `end` send now`, `start` sent with regular start, and `mid` sent for middle stuff (annalise, rachel)
-
-        if(this.gameModule.hasNextRole) {
-          try {
-            this.nextAct();
-          } catch(error) { this.beginVoting(); }
-        } else {
-          console.log("Starting voting phase...");
-          this.cleanUp();
-          this.onVotingBegin();
-        }
-      }, 7000); // TODO: maybe make timer conditional length for Annalise (more time)
-    });
+  cleanUp() {
+    this.wsem.removeEventHandler(events.c_act, this.actHandler);
+    this.wsem.removeEventHandler(events.c_narr_ack, this.narrAckHandler);
   }
 
   nextAct() {
@@ -64,9 +37,42 @@ class GameApp {
     playerTargets.forEach((player, i) => this.wsem.sendMessage(player.id, events.s_act, { data: roleData[i] }));
   }
 
-  cleanUp() {
-    // TODO: make event listeners their own functions
-    // TODO: clear all event listeners this class has used
+  actHandler(id, data) {
+    let responseData;
+    try {
+      responseData = this.gameModule.idAct(id, data.data);
+    } catch(error) {
+      this.wsem.sendMessage(id, events.s_error, { message: error.message });
+    }
+    if(responseData) this.wsem.sendMessage(id, events.s_act, { data: responseData });
+  }
+
+  narrAckHandler() {
+    if(this.actTimer) return;
+
+    this.actTimer = setTimeout(() => {
+      this.actTimer = null;
+
+      if(this.gameModule.currentRole === roles.rachel) {
+        this.gameModule.players.filter((player) => player.originalRole === roles.rachel)
+          .forEach((player) => {
+            if(!player.asleep) this.wsem.sendMessage(player.id, events.s_act, { data: { noise: true } })
+          });
+      }
+
+      // TODO: add `state` prop to `s_act` that is either `start`, `end`, or `mid`
+      // TODO: `end` send now`, `start` sent with regular start, and `mid` sent for middle stuff (annalise, rachel)
+
+      if(this.gameModule.hasNextRole) {
+        try {
+          this.nextAct();
+        } catch(error) { this.beginVoting(); }
+      } else {
+        console.log("Starting voting phase...");
+        this.cleanUp();
+        this.onVotingBegin();
+      }
+    }, 7000); // TODO: maybe make timer conditional length for Annalise (more time)
   }
 }
 

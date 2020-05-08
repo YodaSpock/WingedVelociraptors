44b14ec3fecbaffcea08428c6b9c67072f3e5f89
Isaac Spanier <ispanier@iastate.edu>
2020-05-08 17:12:17 -0500

    my all cards changes

--- client/src/Pages/AllCards.jsx
+++ client/src/Pages/AllCards.jsx
@@ -1,22 +1,66 @@
 import React from 'react';
-import CharacterCard from './CharacterCard';
+import {Checkbox, Divider, Button} from 'antd';
+//import CharacterCard from './CharacterCard';
+import '../Styles/FixMargin.scss'
 
 export default class AllCards extends React.Component{
+    state = {
+        value: 1,
+    }
+
+    onChange = (checkedNames) => {
+        const names = [];
+        console.log(checkedNames);
+        checkedNames.forEach((name) => {
+            console.log("Player: " + name + " Selected")
+        })
+        // this.setState({
+        //     value: e.target.value,
+        // });
+      };
+
     render(){
         const {
             role,
             players,
+            onSubmit
         } = this.props; 
         
+        const radioStyle = {
+            display: 'block',
+            height: '30px',
+            lineHeight: '30px',
+            //marginBottom: '5px',
+        };
+
+        const {value} = this.state;
+
+        {/* {players.map((player) => <p key={player.id}>{player.name}</p>)} */}
+        {/* {players.map((player) => <CharacterCard key = {player.id} flippable = {false} name = {player.name}/> )}
+        {players.map((player) => {console.log(player.name)})} */}
         return(
-            <div>
-                All Cards
-                <br/>
-                Your Role: {role}
-                <br/>
-                {/* {players.map((player) => <p key={player.id}>{player.name}</p>)} */}
-                {players.map((player) => <CharacterCard key = {player.id} flippable = {false} name = {player.name}/> )}
-                {players.map((player) => {console.log(player.name)})}
+            <div style = {{fontFamily: "minecraft", marginLeft: "10vw", marginRight: "10vw"}}>
+                Select a Player: 
+                <Divider/>
+                <div style = {{alignText: "center", height: "100%", width: "100%"}}>
+                    <Checkbox.Group onChange = {this.onChange}>
+                            <Checkbox key = {1} style = {radioStyle} value = {1}>
+                                {1}
+                            </Checkbox> 
+                            <Checkbox key = {2} style = {radioStyle} value = {2}>
+                                {2}
+                            </Checkbox> 
+                            <Checkbox key = {3} style = {radioStyle} value = {3}>
+                                {3}
+                            </Checkbox> 
+                        {/* {players.map((player) => 
+                            <Checkbox key = {player.id} style = {radioStyle} value = {player.id}>
+                                {player.name}
+                            </Checkbox>          
+                        )}           */}
+                    </Checkbox.Group>
+                    <Button onClick = {() => onSubmit()}> Submit </Button>
+                </div>
             </div>
         )
     }

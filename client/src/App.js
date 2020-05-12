38868917818044e87d319b9768e7b7c5c5172e80
danielisgr8 <schubertmdaniel@gmail.com>
2020-05-11 23:25:59 -0500

    Lotsa `localStorage` handling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useEffect } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import Header from './Pages/Header';
@@ -14,18 +14,38 @@ import VotingScreen from "./Pages/VotingScreen";
 import EndGameScreen from "./Pages/EndGameScreen";
 import '../src/Styles/Layout.scss';
 import WebSocketEventManager from './Networking/websocket-event-manager';
+import useStorageState from "./useStorageState";
 
 
 const wsem = new WebSocketEventManager(`ws://${window.location.hostname}:81`);
 // TODO - storing data using useState or useRef - STRETCH GOAL
 
+const isSessionPath = () => ["/", "/player", "/player/end"].every(path => window.location.pathname !== path);
+
 function App() {
 
-  const [role, setRole] = useState("");
-  const [position, setPosition] = useState(0);
-  const [players, setPlayers] = useState([{}]);
-  const [votingData, setVotingData] = useState({});
-  const [killed, setKilled] = useState([]);
+  const [role, setRole] = useStorageState("role", "", isSessionPath());
+  const [position, setPosition] = useStorageState("position", 0, isSessionPath());
+  const [players, setPlayers] = useStorageState("players", [{}], isSessionPath());
+  const [votingData, setVotingData] = useStorageState("votingData", { middle: [] }, isSessionPath());
+  const [killed, setKilled] = useStorageState("killed", [], isSessionPath());
+
+  useEffect(() => {
+    if(isSessionPath() &&
+       localStorage.getItem("clientID") !== null &&
+       localStorage.getItem("sessionID") !== null) {
+      wsem.sendMessage("c_reconnect", {
+        clientID: localStorage.getItem("clientID"),
+        sessionID: localStorage.getItem("sessionID")
+      });
+    } else {
+      localStorage.clear();
+      wsem.addEventHandler("s_init", (data) => {
+        localStorage.setItem("clientID", data.clientID);
+        localStorage.setItem("sessionID", data.sessionID);
+      });
+    }
+  }, []);
   
   const recieveRole = e => {
     setRole(e.role);
@@ -41,6 +61,7 @@ function App() {
     setRole(data.role);
     setPlayers(data.players);
     setKilled(data.killed);
+    localStorage.clear();
   };
 
   return (
